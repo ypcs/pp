70e01495e2e58d6c8737bbe57753fca4506ccbaa
Chris Lamb <chris@chris-lamb.co.uk>
2008-06-21 19:27:29 +0100

    Fix .dsc mangling to match a few other unextractable packages.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- AptFs/download.py
+++ AptFs/download.py
@@ -46,8 +46,8 @@ def download(srcpkg, tempdir=None, secure=False):
                 '--print-uris "%s" | sed -n "s/\'\(http[^\']*.dsc\).*/\\1/p")' % srcpkg,
 
             # Break the signature such that dpkg-source does not attempt to verify it.
-            'sed -i \'1d\' *.dsc',
-            'dpkg-source -x *.dsc unpacked',
+            'awk -v X=0 \'/^[ ]*$/ { X=1 } { if (X) print }\' *.dsc > src.dsc',
+            'dpkg-source -x src.dsc unpacked',
         )
     status, output = commands.getstatusoutput(' && '.join(cmds))
 
