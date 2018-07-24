007614990aa763bf369ac698f9b0b26800f5149d
Chris Lamb <lamby@debian.org>
2018-07-24 18:37:28 +0800

    Releasing version 0.12.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='aptfs',
-    version='0.11.0',
+    version='0.12.0',
     author="Chris Lamb",
     author_email="lamby@debian.org",
     packages=(
