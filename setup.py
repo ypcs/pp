90fea3cbce3680c1cc49e7ec34f27eacdffb117e
Chris Lamb <lamby@debian.org>
2018-07-24 18:47:43 +0800

    Releasing version 0.13.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='aptfs',
-    version='0.12.1',
+    version='0.13.0',
     author="Chris Lamb",
     author_email="lamby@debian.org",
     packages=(
