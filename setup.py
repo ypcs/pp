cfba96f3768a0a774c0dfd1f7bb0ceec0f9fb674
Chris Lamb <lamby@debian.org>
2019-09-09 11:24:31 +0100

    releasing package aptfs version 2:1.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='aptfs',
-    version='0.13.1',
+    version='1.0.0',
     author="Chris Lamb",
     author_email="lamby@debian.org",
     packages=('AptFs',),
